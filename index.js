2c4838a95aeb9b74344be21fd1871c6d11356235
Sahil Chouhan <chouhangolu604@gmail.com>
2025-11-20 11:14:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+const app = express()
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res)=>{
+    res.render('list')
+})
+
+app.get('/add', (req, res)=>{
+    res.render('add')
+})
+
+app.get('/update', (req, res)=>{
+    res.render('update')
+})
+
+app.listen(3200, ()=>console.log('Server is running on Port'))
