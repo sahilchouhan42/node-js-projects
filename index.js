cc9cd792fd46ad9d64f4d13cb8df453a52c408fc
Sahil Chouhan <chouhangolu604@gmail.com>
2025-11-23 18:46:02 +0530

    update

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import express from 'express'
 import path from 'path'
+import { MongoClient } from 'mongodb'
 
 const app = express()
 const publicPath = path.resolve('public')
 
 app.use(express.static(publicPath))
 app.set('view engine', 'ejs')
+
+const dbName = 'node-project'
+const collectionName = 'todo'
+const url = 'mongodb://localhost:27017'
+const client = new MongoClient(url)
+
+const connection = async ()=>{
+    const connect = await client.connect();
+    return await connect.db(dbName)
+}
+
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res)=>{
     res.render('list')
@@ -23,8 +36,15 @@ app.post('/update', (req, res)=>{
     res.redirect('/')
 })
 
-app.post('/add', (req, res)=>{
-    res.redirect('/')
+app.post('/add', async (req, res)=>{
+    const db = await connection()
+    const collection = db.collection(collectionName)
+    const result = collection.insertOne(req.body)
+    if(result){
+        res.redirect('/')
+    } else{
+        res.redirect('/add')
+    }
 })
 
 app.listen(3200, ()=>console.log('Server is running on Port'))
